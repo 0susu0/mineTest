5a1a281d18885334b46fe063ee527fc690b23593
0susu0 <598636164@qq.com>
2020-12-25 01:06:18 +0800

    [A] 25号算法题

---
+++ 2020/12/25.js
@@ -0,0 +1,39 @@
+// 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+
+// 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/two-sum
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
+// 示例:
+
+// 给定 nums = [2, 7, 11, 15], target = 9
+// 因为 nums[0] + nums[1] = 2 + 7 = 9
+// 所以返回 [0, 1]
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let result = []
+    for (let i = 0; i <nums.length; i++) {
+        let res = target - nums[i]
+        let second = nums.indexOf(res)
+        if (second === -1) continue
+        if (second === i) continue
+        result.push.call(result, i, second)
+        break
+    }
+    return result
+}; 
+
+
+let nums = [3,2,4]
+let target = 6
+
+console.log(twoSum(nums, target))
