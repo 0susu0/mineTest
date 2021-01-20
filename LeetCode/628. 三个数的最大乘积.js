732b1d95e1e96f2212d7dfece9d6a00d233eb64a
suleungs <598636164@qq.com>
2021-01-20 18:00:33 +0800

    [A] 628. 三个数的最大乘积 [A] 1584. 连接所有点的最小费用

---
+++ LeetCode/628. 三个数的最大乘积.js
@@ -0,0 +1,28 @@
+// 给定一个整型数组，在数组中找出由三个数组成的最大乘积，并输出这个乘积。
+
+// 示例 1:
+
+// 输入: [1,2,3]
+// 输出: 6
+
+// 示例 2:
+
+// 输入: [1,2,3,4]
+// 输出: 24
+
+// 注意:
+
+// 给定的整型数组长度范围是[3,104]，数组中所有的元素范围是[-1000, 1000]。
+// 输入的数组中任意三个数的乘积不会超出32位有符号整数的范围。
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumProduct = function(nums) {
+  nums = nums.sort((a, b) => {a - b})
+  let len = nums.length
+  return Math.max(nums[len - 1] * nums[len -2] * nums[len - 3], nums[0] * nums[1] * nums[len - 1])
+};
+
+console.log(maximumProduct([-100,-98,-1,2,3,4]))
